3724dff8bd90efe1f51a897c5c8bfaff6fd19a9a
A Ruiz <luis.alonso.16@hotmail.com>
2020-07-25 11:08:28 -0600

    Add material, Fix tailwind PostCSS config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatButtonModule,
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Mulish', 'sans-serif'],
+      body: ['Lato', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        blue: '#0057FF',
+      },
+    },
   },
   variants: {},
   plugins: [],
