e876b84f8a2ff009412d17c3f54e2d7b9744d12c
A Ruiz <luis.alonso.16@hotmail.com>
2020-07-26 11:13:04 -0600

    Add swiper

--- src/app/page/shell/feed/feed.component.ts
+++ src/app/page/shell/feed/feed.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { ThemeService } from '../../../common/service/theme.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import Swiper from 'swiper';
 
 interface Ad {
   title: string;
@@ -17,7 +18,7 @@ const Ads: Array<Ad> = [
   },
   {
     title: 'Learn Astrophysics',
-    description: 'Check out the top authors',
+    description: 'Check out the top authors.',
     image: 'https://images.unsplash.com/photo-1579532040113-c94c4a38a1e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=516&q=80'
   }
 ];
@@ -27,13 +28,42 @@ const Ads: Array<Ad> = [
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss']
 })
-export class FeedComponent implements OnInit, OnDestroy {
+export class FeedComponent implements OnInit, AfterViewInit, OnDestroy {
+  // Observers
   private subject: Subject<void> = new Subject();
+
+  // Data
   public ads = Ads;
+
+  // UI
+  private swiper: Swiper;
+  @ViewChild('adSwiper')
+  private adSwiper: ElementRef;
 
   constructor(private themeService: ThemeService) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.swiper = new Swiper(this.adSwiper.nativeElement, {
+      // cssMode: true,
+      observer: true,
+      slidesPerView: 'auto',
+      // SET FREE MODE IF YOU WANT TO MOVE SWIPER IG STORIE'S LIKE
+      freeMode: false,
+      preloadImages: false,
+      watchSlidesVisibility: false,
+      spaceBetween: 16,
+      // mousewheel: true,
+      lazy: {
+        loadPrevNext: true,
+        loadPrevNextAmount: 3,
+        elementClass: 'swiper-lazy',
+        loadedClass: 'swiper-lazy-loaded',
+        preloaderClass: 'swiper-lazy-preloader',
+      }
+    });
   }
 
   ngOnDestroy(): void {
@@ -42,6 +72,7 @@ export class FeedComponent implements OnInit, OnDestroy {
   }
 
   onClick(): void {
+    this.swiper.slideNext();
     this.themeService.toggle().pipe(takeUntil(this.subject)).subscribe((theme: string) => {
     });
   }
