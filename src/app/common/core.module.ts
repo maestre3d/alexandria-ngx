bd5c700d2212ce69ea0cddd6d503c9e95af60d31
A Ruiz <luis.alonso.16@hotmail.com>
2020-07-28 17:10:47 -0600

    Add ngrx store

--- src/app/common/core.module.ts
+++ src/app/common/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forRoot({ })
   ]
 })
 export class CoreModule { }
