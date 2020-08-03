0045d30219043ae27a54fec58824847ce0426808
A ruiz <luis.alonso.16@hotmail.com>
2020-08-03 04:24:01 -0600

    Update search page

--- src/app/page/shell/search/search.component.ts
+++ src/app/page/shell/search/search.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
+  // Data
+  public query: string;
+  // RxJS
+  private subject$: Subject<void> = new Subject();
+  // UI
+  public isSearching = false;
 
-  constructor() { }
+  constructor(private activeRoute: ActivatedRoute, private router: Router,
+              private title: Title) {
+  }
 
   ngOnInit(): void {
+    this.activeRoute.queryParamMap.pipe(takeUntil(this.subject$)).subscribe((param) => {
+      this.query = param.get('q');
+      if (this.query && this.query !== '') {
+        this.title.setTitle(`${this.query} • Alexandria Search`);
+      }
+    }, (err) => {
+      console.error(err);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subject$.next();
+    this.subject$.complete();
+  }
+
+  onQuery(event: KeyboardEvent): void {
+    if (this.query !== '') {
+      setTimeout(() => {
+        if (!this.isSearching) {
+          this.isSearching = true;
+          this.router.navigate(['/search'], { queryParams: {q: this.query} });
+          return;
+        }
+
+        this.isSearching = false;
+      }, 1000);
+    }
   }
 
 }
--- src/app/page/shell/search/search.module.ts
+++ src/app/page/shell/search/search.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchComponent } from './search.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '@alexandria/common/shared.module';
 
 
 @NgModule({
   declarations: [SearchComponent],
   imports: [
     CommonModule,
+    RouterModule,
+    SharedModule,
     SearchRoutingModule
   ]
 })
