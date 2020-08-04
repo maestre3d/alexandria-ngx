998d0e74c789919105cbd687299174e86c535313
Alonso R <luis.alonso.16@hotmail.com>
2020-08-03 22:33:41 -0600

    Set typesafe theme reducer

--- src/app/common/store/reducer/theme.reducer.ts
+++ src/app/common/store/reducer/theme.reducer.ts
@@ -5,12 +5,13 @@ import { ThemeKind } from '../../enum/theme.enum';
 const persistedTheme = localStorage.getItem('prefers-color');
 export const initialState = persistedTheme === ThemeKind.Dark ? persistedTheme : ThemeKind.Light;
 
-const _themeReducer = createReducer(initialState,
+const themeReducerFunc = createReducer(
+    initialState,
     on(toggle, state => state === ThemeKind.Light ? ThemeKind.Dark : ThemeKind.Light),
     on(inject, (state, { theme }) => theme === ThemeKind.Light ? theme : state),
     on(reset, state => ThemeKind.Light)
 );
 
-export function themeReducer(state: ThemeKind, action: Action) {
-    return _themeReducer(state, action);
+export function themeReducer(state: ThemeKind, action: Action): ThemeKind {
+    return themeReducerFunc(state, action);
 }
