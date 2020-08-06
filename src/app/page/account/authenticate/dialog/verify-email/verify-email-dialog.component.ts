f3534270d4cac42aa70f65f45efc16efa05e1e26
Alonso R <luis.alonso.16@hotmail.com>
2020-08-06 15:47:36 -0600

    Add required validator to email verify dialog

--- src/app/page/account/authenticate/dialog/verify-email/verify-email-dialog.component.ts
+++ src/app/page/account/authenticate/dialog/verify-email/verify-email-dialog.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { FormControl, } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-verify-email-dialog',
   templateUrl: './verify-email-dialog.html'
 })
 export class VerifyEmailDialogComponent implements OnInit {
-  public codeForm = new FormControl('');
+  public codeForm = new FormControl('', Validators.required);
   public isPwdHidding = true;
 
   constructor(public dialogRef: MatDialogRef<VerifyEmailDialogComponent>) { }
