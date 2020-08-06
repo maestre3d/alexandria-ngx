680f8c15e5e8d753e01345b86b17717acb925e43
Alonso R <luis.alonso.16@hotmail.com>
2020-08-05 20:28:42 -0600

    Update AWS Cognito sevice

--- src/app/common/service/auth/auth.service.ts
+++ src/app/common/service/auth/auth.service.ts
@@ -1,58 +1,110 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IUser } from '@alexandria/domain/entity/user.entity';
-import { Users } from '@alexandria/mock/user.mock';
-import { CognitoUserPool, CognitoUser, AuthenticationDetails, CookieStorage, CognitoUserSession } from 'amazon-cognito-identity-js';
+import { CognitoUserPool, CognitoUser, CognitoUserSession } from 'amazon-cognito-identity-js';
 import { environment } from 'environments/environment';
-import { Config } from '@alexandria/config/alexandria.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private removePolicy = new Date();
-
-  private storage = new CookieStorage({
-    domain: Config.Domain,
-    expires: this.removePolicy.getTime(),
-    path: '/',
-    secure: true
-  });
-  private userPool = new CognitoUserPool({
-    UserPoolId: environment.cognitoPoolID,
-    ClientId: environment.cognitoClientID,
-    Storage: this.storage
-  });
+  private userPool: CognitoUserPool;
 
   constructor() {
-    this.removePolicy.setDate(new Date().getMonth() + 1);
+    this.removePolicy.setDate(this.removePolicy.getDate() + 30);
+
+    this.userPool = new CognitoUserPool({
+      UserPoolId: environment.cognitoPoolID,
+      ClientId: environment.cognitoClientID
+    });
   }
 
-  getUserPool(): CognitoUserPool {
-    return this.userPool;
+  getPool(): Observable<CognitoUserPool> {
+    return new Observable(observer => {
+      observer.next(this.userPool);
+      observer.complete();
+    });
   }
 
-  getCognitoUser(username: string): CognitoUser {
+  getCurrentUser(): CognitoUser {
+    return this.userPool.getCurrentUser();
+  }
+
+  generateUser(username: string): CognitoUser {
     return new CognitoUser({
       Username: username,
-      Storage: this.storage,
       Pool: this.userPool
     });
   }
 
   getLogged(): Observable<IUser> {
     return new Observable((observer) => {
-      let user: IUser;
-      Users.forEach((u) => {
-        if (u.id === 'ca0770b6-7650-4a0e-b924-aa0396d953ac') {
-          user = u;
-          observer.next(user);
-          observer.complete();
-          return;
-        }
-      });
+      const cognitoUser = this.getCurrentUser();
+      if (cognitoUser !== null) {
+        // Required session to get attributes
+        cognitoUser.getSession((errSession: Error, session: CognitoUserSession) => {
+          if (errSession !== null) {
+            observer.error(errSession);
+            observer.complete();
+            return;
+          }
 
-      observer.error(new Error('user not found'));
+          cognitoUser.getUserData((err, userData) => {
+            if (err !== null) {
+              observer.error(err);
+              observer.complete();
+              return;
+            }
+
+            // Init Mapping process
+            const user: IUser = {
+              id: '',
+              username: userData.Username,
+              email: ''
+            };
+
+            userData.UserAttributes.forEach((v, i) => {
+              switch (v.Name) {
+                case 'email':
+                  user.email = v.Value;
+                  break;
+                case 'sub':
+                  user.id = v.Value;
+                  break;
+                case 'locale':
+                  user.locale = v.Value;
+                  break;
+                case 'picture':
+                  user.image = v.Value;
+                  break;
+                case 'update_at':
+                  user.updateTime = new Date(v.Value);
+                  break;
+                case 'name':
+                  user.name = v.Value;
+                  break;
+                case 'middle_name':
+                  user.middleName = v.Value;
+                  break;
+                case 'given_name':
+                  user.lastName = v.Value;
+                  break;
+              }
+            });
+
+            // Set display name (null | name | name + last_name | name + middle_name + last_name)
+            user.displayName = user.name && user.name !== '' ? user.lastName && user.lastName !== '' ?
+              user.middleName && user.middleName !== '' ? `${user.name} ${user.middleName} ${user.lastName}` :
+              `${user.name} ${user.lastName}` : user.name : null;
+
+            observer.next(user);
+            observer.complete();
+          });
+        });
+      }
+
+      observer.next(null);
       observer.complete();
     });
   }
